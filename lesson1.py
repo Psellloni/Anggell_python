fe3500128eb1eae5993c32a631be2f0eda375958
Pselloni <kasenkolev318@gmail.com>
2025-04-17 17:17:14 +0300

    first commit

---
+++ lesson1.py
@@ -0,0 +1,66 @@
+
+# nadya_height = 170 
+# nadya_weight = 150.6
+# is_female = True 
+# is_slim = False
+# x = None 
+# imt = None
+
+# imt = nadya_weight / nadya_height
+# imt2 = nadya_weight // nadya_height
+# imt3 = 12 % 5
+# imt4 = 25 ** -1
+
+# print(imt2)
+
+# str1 = "hello world"
+# str2 = 'hello world2'
+# str3 = "Моя любимая книга 'Букварь'"
+# str4 = 'Моя любимая книга "Букварь"'
+
+# str1 = "hello worldhe"
+
+# print(len(str1))
+# print(str1.count("kxckvjxcv"))
+
+# print(str1.coount("jsdhjdf"))
+
+# print(str1)
+
+# str1, str2 = "hello world", "hello world"
+
+# print(str1[2]) 
+# print(str1[-1])
+# print(str2)
+# print("mamaaaaaa")
+
+# mas = [12, 13, 190]
+# mas2 = [12, 13.88, "190"]
+# mas3 = [12, 13, 1, 1, 56, 34, 78, 34, 122]
+
+# print(mas3[::-1])
+
+# mas = [1, 2, 3, 4, 5, 2, 6]
+
+# mas.pop()
+
+# print(mas)
+
+# tup = (1, 2, 3, 4, 5)
+# tup2 = 3, 6, 5, 12, 34
+
+# print(type(tup))
+
+
+# x = [0] * 1000
+
+# print(x)
+
+x = input()
+
+if x.isdigit() == True:
+    x = int(x)
+    print("Успешно изменили формат")
+else:
+    print("Данная строка не подходит")
+print('kljsahdfjhashdfasf')
---
+++ lesson2.py
@@ -0,0 +1,64 @@
+# x = 15
+
+# if x % 5 == 0:
+#     print('Делится на 5')
+# elif x % 5 == 1:
+#     print('Остаток 1')
+# else:
+#     print('Остаток 2, 3, 4, 5')
+
+# if x % 5 == 0:
+#     print('Делится на 5')
+
+
+# if x % 5 == 1:
+#     print('Остаток 1')
+# else:
+#     print('Остаток 2, 3, 4, 5')
+
+# x = 0
+
+# while x != 100:
+#     x += 1
+#     print(x)
+
+# while True:
+#     text = input("Пользователь: ")
+
+#     text = text.lower()
+
+#     if "прив" in text:
+#         print("Бот: Привет!")
+#     elif ("стоп" in text) or ("хватит" in text):
+#         print("Bye-bye")
+#         break
+#     else:
+#         print("Бот: Чё сказал?")
+
+# x = 0
+    
+# while x != 20:
+
+#     x += 1
+
+#     print(x)
+
+#     if x % 5 == 0:
+#         continue
+
+#     print("Биба и Боба")
+
+# mas = [1, 2, 3, 4, 5, 1, 23, 12, 3]
+
+
+# for value in :
+#     print(value)
+
+# x = list(range(1, 12, 2))
+
+# print(x)
+
+mas = [1, 2, 3, 4, 5, 1, 23, 12, 3]
+
+for i in range(len(mas)):
+    print(mas[i])
